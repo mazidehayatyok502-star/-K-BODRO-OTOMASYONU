0487079a8b06890aeb1f727701ef19390707e49b
mazidehayatyok502-star <24430040031@mersin.edu.tr>
2026-05-02 15:28:54 +0300

    tootallnate

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-module.exports = {
-  ...require('./fs.js'),
-  copyFile: require('./copy-file.js'),
-  cp: require('./cp/index.js'),
-  mkdir: require('./mkdir/index.js'),
-  mkdtemp: require('./mkdtemp.js'),
-  rm: require('./rm/index.js'),
-  withTempDir: require('./with-temp-dir.js'),
-  writeFile: require('./write-file.js'),
+"use strict";
+function noop() { }
+function once(emitter, name) {
+    const o = once.spread(emitter, name);
+    const r = o.then((args) => args[0]);
+    r.cancel = o.cancel;
+    return r;
 }
+(function (once) {
+    function spread(emitter, name) {
+        let c = null;
+        const p = new Promise((resolve, reject) => {
+            function cancel() {
+                emitter.removeListener(name, onEvent);
+                emitter.removeListener('error', onError);
+                p.cancel = noop;
+            }
+            function onEvent(...args) {
+                cancel();
+                resolve(args);
+            }
+            function onError(err) {
+                cancel();
+                reject(err);
+            }
+            c = cancel;
+            emitter.on(name, onEvent);
+            emitter.on('error', onError);
+        });
+        if (!c) {
+            throw new TypeError('Could not get `cancel()` function');
+        }
+        p.cancel = c;
+        return p;
+    }
+    once.spread = spread;
+})(once || (once = {}));
+module.exports = once;
+//# sourceMappingURL=index.js.map
